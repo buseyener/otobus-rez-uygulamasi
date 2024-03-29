12320f0f5fb86a1d753a3e5fb6494fa69411f333
Buse Yener <119698903+buseyener@users.noreply.github.com>
2024-03-29 17:45:17 +0300

    Otobus Rezervasyon Web Uygulamasi

---
+++ Otobus_Rez_Uygulamasi/vendor/inc/nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Umuttepe Turizm</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">Hakkında</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.php">Servis</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">İletişim</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Galeri</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Login Panel
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="admin/">Admin Giriş</a>
+              <a class="dropdown-item" href="usr/">Müşteri Giriş</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
